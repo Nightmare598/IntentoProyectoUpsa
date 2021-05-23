eea2d4624ec7be48c15e758a32626992550fa02f
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-05-22 21:16:26 -0400

    Adicion del modelo

---
+++ Models/Paciente.cs
@@ -0,0 +1,12 @@
+﻿namespace IntentoProyectoUpsa.Models
+{
+    public class Paciente
+    {
+        public int idPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public int Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
