bcbd250a0b4d8b5d8ec639f32e9c4e0edb64d68f
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-05-22 21:36:52 -0400

    Creacion del Controlador PacientesController

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IntentoProyectoUpsa.Models;
+
+namespace IntentoProyectoUpsa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        private readonly PacienteContext _context;
+
+        public PacientesController(PacienteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pacientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Paciente>>> GetPacientes()
+        {
+            return await _context.Pacientes.ToListAsync();
+        }
+
+        // GET: api/Pacientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Paciente>> GetPaciente(long id)
+        {
+            var paciente = await _context.Pacientes.FindAsync(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return paciente;
+        }
+
+        // PUT: api/Pacientes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaciente(long id, Paciente paciente)
+        {
+            if (id != paciente.idPaciente)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paciente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PacienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pacientes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Paciente>> PostPaciente(Paciente paciente)
+        {
+            _context.Pacientes.Add(paciente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaciente", new { id = paciente.idPaciente }, paciente);
+        }
+
+        // DELETE: api/Pacientes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaciente(long id)
+        {
+            var paciente = await _context.Pacientes.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pacientes.Remove(paciente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PacienteExists(long id)
+        {
+            return _context.Pacientes.Any(e => e.idPaciente == id);
+        }
+    }
+}
