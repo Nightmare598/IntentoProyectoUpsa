3cab915198e53b85c03c8b850b7b23b20021be50
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-05-22 21:20:55 -0400

    Adicion de paquete Nugget y del contexto

---
+++ Models/PacienteContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntentoProyectoUpsa.Models
+{
+    public class PacienteContext : DbContext
+    {
+        public PacienteContext(DbContextOptions<PacienteContext> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+    }
+}
